9836ef6eab6660276ca514a49c6d22e4c59314c3
rorycl <gitrl@campbell-lange.net>
2024-04-22 14:10:55 +0100

    cex: improve module comments

--- cex.go
+++ cex.go
@@ -1,8 +1,13 @@
 // Package cexfind searches for devices for sale at Cex/Webuy via the
-// unofficial `webuy.io` query endpoint which responds in a json format.
+// unofficial "webuy.io" query endpoint which responds in a json format.
 //
 // Queries are required to be made in the UK as the endpoint is
 // protected by region-sensitive CDN.
+//
+// Multiple concurrent queries are supported, with an optional "strict"
+// flag to constrain results to the query terms. The results are a union
+// of the results of each query, ordered by model name and then the
+// price of each item.
 //
 // Example usage:
 //
@@ -11,8 +16,18 @@
 //		log.Fatal(err)
 //	}
 //
+//	latestModel := ""
 //	for _, box := range results {
-//		fmt.Printf("%20s : %3d %s\n", box.Model, box.Price, box.Name)
+//		if box.Model != latestModel {
+//			fmt.Printf("\n%s\n", box.Model)
+//			latestModel = box.Model
+//		}
+//		fmt.Printf(
+//			"   £%3d %s\n   %s\n",
+//			box.Price,
+//			box.Name,
+//			box.IDUrl(),
+//		)
 //	}
 package cexfind
 
@@ -23,8 +38,9 @@ import (
 	"strings"
 )
 
-// Box is a rationalised representation of a Cex/Webuy json entry, where
-// each entry represents a "Box" or computer or other item for sale.
+// Box is a very simplified representation of a Cex/Webuy json entry,
+// where each entry represents a "Box" or computer or other item for
+// sale.
 type Box struct {
 	Model string
 	Name  string
@@ -35,7 +51,7 @@ type Box struct {
 // inQuery checks to see if each of the words in at least one of the
 // supplied queries are in the Name of a Box. inQuery is used for
 // determining if a particular Box should be returned from a "strict"
-// Search
+// search.
 func (b *Box) inQuery(queries []string) bool {
 	for _, q := range queries {
 		matches := 0
@@ -53,8 +69,8 @@ func (b *Box) inQuery(queries []string) bool {
 	return false
 }
 
-// IDUrl returns the full url path to the Cex/Webuy webpage showing Box
-// in question
+// IDUrl returns the full url path to the Cex/Webuy webpage showing the
+// Box (the item of equipment) in question.
 func (b Box) IDUrl() string {
 	return urlDetail + b.ID
 }
@@ -109,10 +125,14 @@ type boxResults struct {
 }
 
 // Search searches the Cex json endpoint at URL for the provided
-// queries, returning a slice of Box or error. The strict flag ensures
-// that the results contain terms from the search queries as the
-// non-strict results include additional suggestions from the
-// Cex/Webuy system.
+// queries, returning a slice of Box or error.
+//
+// The strict flag ensures that the results contain terms from the
+// search queries as the non-strict results include additional
+// suggestions from the Cex/Webuy system.
+//
+// Multiple queries are run concurrently and their results aggregated
+// and sorted by model, then by price ascending.
 func Search(queries []string, strict bool) ([]Box, error) {
 
 	var allBoxes []Box
